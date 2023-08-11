55ad2e86aa710a404d7b66a83bc7616dfdb2c932
Radost Koleva <radostkoleva0142@gmail.com>
2023-08-11 14:10:03 +0300

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+
+    private int a;
+    private int b;
+
+    public Calculator(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+}
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
+public class TestCalculator {
+
+    @ParameterizedTest
+    @CsvSource(value = {"10, 2"})
+    void testSum(int a, int b) {
+        int expectedResult = 12;
+        int actualResult = Calculator.sum(a, b);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"10, 2"})
+    void testMultiply(int a, int b) {
+        int expectedResult = 20;
+        int actualResult = Calculator.multiply(a, b);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"10, 2"})
+    void testDivide(int a, int b) {
+        int expectedResult = 5;
+        int actualResult = Calculator.divide(a, b);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"10, 2"})
+    void testSubtract(int a, int b) {
+        int expectedResult = 8;
+        int actualResult = Calculator.subtract(a, b);
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+}
